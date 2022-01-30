642a168e277a7b798e0c649921e1aaf00f9582d4
eliascanaza <sistemc44@gmail.com>
2022-01-30 17:11:49 -0600

    add @NotBlank in entities and change Comment in DB

--- comment-microservice/src/main/java/cs544/domain/Comment.java
+++ comment-microservice/src/main/java/cs544/domain/Comment.java
@@ -1,4 +1,5 @@
 package cs544.domain;
+
 import java.util.Date;
 
 import javax.persistence.*;
@@ -6,37 +7,35 @@ import javax.validation.constraints.NotBlank;
 
 import lombok.*;
 
-@Entity(name="Comments")
-@NoArgsConstructor @Getter @Setter
+@Entity(name = "Comment")
+@NoArgsConstructor
+@Getter
+@Setter
 @AllArgsConstructor
 public class Comment {
 	@Id
 	@GeneratedValue
 	private Integer id;
 	@NotBlank
-	private String content;	
+	private String content;
+	@NotBlank
 	@Temporal(TemporalType.DATE)
 	private Date commentedOn;
+	@NotBlank
 	private Boolean active;
+	@NotBlank
 	private Integer userId;
+	@NotBlank
 	private Integer postId;
-	
-//	public Comment(String content, Date postedOn, Boolean active, Integer userId, Integer postId) {
-//		super();
-//		this.content = content;
-//		this.postedOn = postedOn;
-//		this.active = active;
-//		this.userId = userId;
-//		this.postId = postId;
-//	}
+
 	public Comment(String content, Date postedOn, Boolean active, Integer userId, Integer postId) {
 		this(null, content, postedOn, active, userId, postId);
 	}
 
 	@Override
 	public String toString() {
-		return "Comment [id=" + id + ", content=" + content + ", postedOn=" + commentedOn + ", active="
-				+ active + ", userId=" + userId + ", postId=" + postId + "]";
+		return "Comment [id=" + id + ", content=" + content + ", postedOn=" + commentedOn + ", active=" + active
+				+ ", userId=" + userId + ", postId=" + postId + "]";
 	}
 
 }
--- post-microservice/src/main/java/cs544/domain/Post.java
+++ post-microservice/src/main/java/cs544/domain/Post.java
@@ -7,79 +7,40 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
 public class Post {
 	@Id
 	@GeneratedValue
 	private Integer id;
+	@NotBlank
 	private String title;
+	@NotBlank
 	private String content;
-	
+	@NotBlank
 	@Temporal(TemporalType.DATE)
 	private Date postedOn;
+	@NotBlank
 	private Boolean active;
-	
+	@NotBlank
 	private Integer userId;
 
-	public Post() {
-		
-	}
-	
 	public Post(String title, String content, Date postedOn, boolean active, Integer user_id) {
 		super();
 		this.title = title;
 		this.content = content;
 		this.postedOn = postedOn;
 		this.active = active;
-		this.userId = user_id;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public Date getPostedOn() {
-		return postedOn;
-	}
-
-	public void setPostedOn(Date postedOn) {
-		this.postedOn = postedOn;
-	}
-
-	public boolean isActive() {
-		return active;
-	}
-
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-
-	public Integer getUser_id() {
-		return userId;
-	}
-
-	public void setUser_id(Integer user_id) {
 		this.userId = user_id;
 	}
 
