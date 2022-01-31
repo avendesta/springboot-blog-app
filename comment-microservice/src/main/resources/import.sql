257cad6b0a418c50b71bf0a4e5f6631274142fcb
avendesta <53615807+avendesta@users.noreply.github.com>
2022-01-31 08:19:37 -0600

    fixed some errors and did validation

--- comment-microservice/src/main/resources/import.sql
+++ comment-microservice/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO COMMENT VALUES(NULL, true, LOCALTIMESTAMP(), "first twitter comment content", 1, 1);
+INSERT INTO COMMENT VALUES(NULL, true, NOW(), "first twitter comment content", 1, 1);
 INSERT INTO COMMENT VALUES(NULL, true, NOW(), "second twitter comment content", 1, 2);
 INSERT INTO COMMENT VALUES(NULL, true, NOW(), "third twitter comment content", 1, 2);
 INSERT INTO COMMENT VALUES(NULL, true, NOW(), "fourth twitter comment content", 2, 1);
